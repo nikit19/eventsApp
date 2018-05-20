24182fa546e37bf69bbeb9c2089104570892b51f
Sanyog Ghosh <f2016635@goa.bits-pilani.ac.in>
2018-05-21 01:46:22 +0530

    Few Modifications  (#11)
    * a
    
    * picasso based image loading done, dimens and strings modified and ui changed a bit

--- app/src/main/java/com/example/nikit/eventsapp/EventsRecyclerAdapter.java
+++ app/src/main/java/com/example/nikit/eventsapp/EventsRecyclerAdapter.java
@@ -4,6 +4,7 @@ package com.example.nikit.eventsapp;
  * Created by harsimar on 20/05/18.
  */
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +12,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.nikit.eventsapp.model.Attributes;
 import com.example.nikit.eventsapp.model.Event;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,7 @@ public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAd
 
     public static class EventViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
+        ImageView eventImage;
         TextView eventNameTv,eventStartsAt,desciption;
 
         EventViewHolder(View itemView) {
@@ -30,8 +34,8 @@ public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAd
             cv = (CardView)itemView.findViewById(R.id.all_events_card);
             eventNameTv = (TextView)itemView.findViewById(R.id.all_events_card_event_name);
             eventStartsAt = (TextView)itemView.findViewById(R.id.all_events_card_starts_at);
+            eventImage = (ImageView)itemView.findViewById(R.id.all_events_card_image);
             desciption = (TextView)itemView.findViewById(R.id.description);
-
         }
     }
     List<Event> events;
@@ -58,6 +62,14 @@ public class EventsRecyclerAdapter extends RecyclerView.Adapter<EventsRecyclerAd
         holder.eventNameTv.setText(attributes.getName());
         holder.eventStartsAt.setText(attributes.getStartsAt().substring(0,9));
         holder.desciption.setText(attributes.getDesciption());
+
+        //Picasso
+        if(attributes.getOriginalImageUrl() != null) {
+            Picasso.with(holder.eventImage.getContext())
+                    .load(Uri.parse(attributes.getOriginalImageUrl()))
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(holder.eventImage);
+        }
 
         Log.d("harsimarSingh","Setting "+events.get(position).getAttributes().getOrganizerDescription());
     }
--- app/src/main/java/com/example/nikit/eventsapp/model/Attributes.java
+++ app/src/main/java/com/example/nikit/eventsapp/model/Attributes.java
@@ -15,6 +15,8 @@ public class Attributes {
     private String organizerDescription;
     @SerializedName("starts-at")
     private String startsAt;
+    @SerializedName("original-image-url")
+    private String originalImageUrl;
     @SerializedName("description")
     private String desciption;
 
@@ -37,4 +39,6 @@ public class Attributes {
     public String getOrganizerDescription() {
         return organizerDescription;
     }
+
+    public String getOriginalImageUrl() {return originalImageUrl; }
 }
