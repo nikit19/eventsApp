10703bf2831a2a42cc74304c7e1e89c8ac4202cb
Harsimar <simarsingh24@gmail.com>
2018-05-23 18:53:44 +0530

    added lombok

--- app/src/main/java/com/example/nikit/eventsapp/model/User.java
+++ app/src/main/java/com/example/nikit/eventsapp/model/User.java
@@ -2,15 +2,16 @@ package com.example.nikit.eventsapp.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 /**
  * Created by harsimar on 20/05/18.
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
 
 public class User {
     @SerializedName("data")
     private UserData user;
-
-    public UserData getUser() {
-        return user;
-    }
 }
